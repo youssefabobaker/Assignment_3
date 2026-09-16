ccd77dbcb3325ed6ccc3c17d93b2a6007a6e49b8
youssefabobaker <youssefabobaker90@gmail.com>
2026-09-16 15:38:33 +0300

    question1

--- Assignment_3/Program.cs
+++ Assignment_3/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price(index 1)
+            double[] prices = { 25.5, 40.0, 33.75 };
+            Console.WriteLine(prices[1]);
+            #endregion
         }
     }
 }
